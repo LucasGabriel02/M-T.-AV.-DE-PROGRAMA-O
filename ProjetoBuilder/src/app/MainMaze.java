7391408e5786a8f20ab7821b943d08a4a6d6dec4
LucasGabriel02 <54295285+LucasGabriel02@users.noreply.github.com>
2020-04-14 18:30:53 -0300

    Update MainMaze.java

--- ProjetoBuilder/src/app/MainMaze.java
+++ ProjetoBuilder/src/app/MainMaze.java
@@ -11,22 +11,13 @@ import model.Room;
 public class MainMaze {
 	
 	
-	public static void main(String args[]){
-		MazeGame mazeg = new MazeGame ("2", "22", "6", "6", "33");
-		mazeg.makeMaze();
-		
-		Room room = mazeg.Room();
-		
-		
-		
-		
-		
-		System.out.println("		room.makeLabirinto();\r\n" + "room.makeParede();\r\n" + "room.makeQuarto();\r\n" + "room.makePorta();");
-		
-		System.out.println("room.makeMaze();\r\n" + "room.makeWall();\r\n" + "room.makeRoom();\r\n" + "room.makeDoor();\r\n" + "");
+	public static void main(String[] args) {
+			
+			MazeGame mazeg = new MazeGame ("Encantado", "Parede de Encantada", "Quarto da Luz", "Porta Fechada");
+			
+			
+			System.out.println(mazeg);
+
 
 	}
-
-
-	
 }
