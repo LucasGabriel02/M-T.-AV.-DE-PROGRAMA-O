d9806b5ba5c7cfd4c2073a06e6e0df7d496c99ce
LucasGabriel02 <54295285+LucasGabriel02@users.noreply.github.com>
2020-04-14 18:29:49 -0300

    Update Room.java

--- ProjetoBuilder/model/model/Room.java
+++ ProjetoBuilder/model/model/Room.java
@@ -2,45 +2,63 @@ package model;
 
 public class Room {
 	
-	private int totalPorta;
-	private int totalParede;
-	private int totalQuarto;
-
+	private String Labirinto;
+	private String Parede;
+	private String Quarto;
+	private String Porta;
 	
-	public Room(String totalPorta, int totalParede, int totalQuarto,) {
+	public MazeGame(){};
+
+	public MazeGame(String labirinto, String parede, String quarto, String porta) {
 		super();
-		TotalPorta = totalPorta;
-		
-		this.totalPorta = totalPorta;
-		this.totalParede = totalParede;
-		this.totalQuarto = totalQuarto;
+		Labirinto = labirinto;
+		Parede = parede;
+		Quarto = quarto;
+		Porta = porta;
+	}
+
+	public MazeGame(String string, String string2, String string3, String string4, String string5) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getLabirinto() {
+		return Labirinto;
+	}
+
+	public void setLabirinto(String labirinto) {
+		Labirinto = labirinto;
+	}
+
+	public String getParede() {
+		return Parede;
+	}
+
+	public void setParede(String parede) {
+		Parede = parede;
+	}
+
+	public String getQuarto() {
+		return Quarto;
+	}
+
+	public void setQuarto(String quarto) {
+		Quarto = quarto;
+	}
+
+	public String getPorta() {
+		return Porta;
+	}
+
+	public void setPorta(String porta) {
+		Porta = porta;
+	}
+
+	@Override
+	public String toString() {
+		return "MazeGame [Labirinto=" + Labirinto + ", Parede=" + Parede + ", Quarto=" + Quarto + ", Porta=" + Porta
+				+ "]";
+	}
 	
-	}
-
-
-	public int getTotalPorta() {
-		return totalPorta;
-	}
-
-	public void setTotalPorta(int totalPorta) {
-		this.totalPorta = totalPorta;
-	}
-
-	public int getTotalParede() {
-		return totalParede;
-	}
-
-	public void setTotalParede(int totalParede) {
-		this.totalParede = totalParede;
-	}
-
-	public int getTotalQuarto() {
-		return totalQuarto;
-	}
-
-	public void setTotalQuarto(int totalQuarto) {
-		this.totalQuarto = totalQuarto;
-	}
 	
 
 }
