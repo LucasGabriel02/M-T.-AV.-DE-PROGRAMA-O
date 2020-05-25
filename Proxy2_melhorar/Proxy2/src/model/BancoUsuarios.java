fb40d4f2350243757aec710af9be5bebc3000dd1
LucasGabriel02 <54295285+LucasGabriel02@users.noreply.github.com>
2020-05-25 19:39:19 -0300

    Update BancoUsuarios.java

--- Proxy2_melhorar/Proxy2/src/model/BancoUsuarios.java
+++ Proxy2_melhorar/Proxy2/src/model/BancoUsuarios.java
@@ -1,7 +1,8 @@
 package model;
 
 //abstract ou interface
-public class BancoUsuarios {
+
+public abstract class BancoUsuarios implements BancoUsuarioImplementa {
 	
 	
 	private BancoUsuarioImplementa BancoUsuarios;
@@ -16,5 +17,13 @@ public class BancoUsuarios {
 
 	public String getSenha() {
 		return new String("Senha: " + getSenha());
+	}
+
+	public BancoUsuarioImplementa getBancoUsuarios() {
+		return BancoUsuarios;
+	}
+
+	public void setBancoUsuarios(BancoUsuarioImplementa bancoUsuarios) {
+		BancoUsuarios = bancoUsuarios;
 	}
 }
